b5ca54d762e8ab69b18ac2abe1b77e7982802a36
Sakelig <71970061+Sakelig@users.noreply.github.com>
2021-10-31 11:45:44 +0100

    Main method @HttpServer hosts the html sites. Added HttpClient to be able to test server + a few test for client and server. 👍

---
+++ src/test/java/no/kristiania/http/HttpServerTest.java
@@ -0,0 +1,24 @@
+package no.kristiania.http;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class HttpServerTest {
+
+    private final HttpServer server = new HttpServer(0);
+
+    public HttpServerTest() throws IOException {
+    }
+
+    @Test
+    void shouldGetIndexHTMLFromDisk() throws IOException {
+        server.setRoot(Paths.get("src/main/resources"));
+        HttpClient client = new HttpClient("localhost", server.getActualPort(), "/index.html");
+        assertEquals(200, client.getStatusCode());
+    }
+
+}
